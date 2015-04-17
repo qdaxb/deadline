18b4f0cbbad9ebdb3e2c0564a6e6b4c3bd4fcb41
苏星宇 <xingyu3@staff.sina.com.cn>
2015-04-17 16:46:05 +0800

    init project

---
+++ src/main/java/com/weibo/hackathon/deadline/model/Element.java
@@ -0,0 +1,8 @@
+package com.weibo.hackathon.deadline.model;
+
+import com.weibo.hackathon.deadline.scene.SceneManager.Location;
+
+public class Element {
+    public Location loc;
+    public char[][] shape;
+}
---
+++ src/main/java/com/weibo/hackathon/deadline/model/Layer.java
@@ -0,0 +1,7 @@
+package com.weibo.hackathon.deadline.model;
+
+import java.util.ArrayList;
+
+public class Layer {
+    public ArrayList<Element> elements;
+}
