a61bce4eb5bda6066fc63b5771a368a97861dea8
zilong6 <zilong6@staff.weibo.com>
2015-04-17 17:09:54 +0800

    define methods for GameController and GameEngine

--- src/main/java/com/weibo/hackathon/deadline/engine/GameEngine.java
+++ src/main/java/com/weibo/hackathon/deadline/engine/GameEngine.java
@@ -1,6 +1,10 @@
 package com.weibo.hackathon.deadline.engine;
 
+import com.weibo.hackathon.deadline.engine.flow.GameResult;
+
 public interface GameEngine {
+    
+    public void reportResult(GameResult result);
 
     
 }
--- src/main/java/com/weibo/hackathon/deadline/engine/flow/GameController.java
+++ src/main/java/com/weibo/hackathon/deadline/engine/flow/GameController.java
@@ -1,5 +1,14 @@
 package com.weibo.hackathon.deadline.engine.flow;
 
-public interface GameController {
+import com.weibo.hackathon.deadline.engine.GameEngine;
+import com.weibo.hackathon.deadline.engine.input.GameInput;
 
+public interface GameController {
+    
+    public void init();
+    
+    public void input(GameInput input);
+    
+    public void setGameEngine(GameEngine engine);
+    
 }
