d3ab19b0e85536c6438558ce511cc326fbd26b70
axb <qindi@staff.sina.com.cn>
2015-04-17 17:09:40 +0800

    update

--- src/main/java/com/weibo/hackathon/deadline/engine/GameEngine.java
+++ src/main/java/com/weibo/hackathon/deadline/engine/GameEngine.java
@@ -1,6 +1,20 @@
 package com.weibo.hackathon.deadline.engine;
 
-public interface GameEngine {
+import com.weibo.hackathon.deadline.engine.flow.GameController;
+import com.weibo.hackathon.deadline.engine.input.InputManager;
+import com.weibo.hackathon.deadline.scene.SceneManager;
 
+/**
+ * 游戏引擎入口
+ */
+public class GameEngine {
+
+
+    private GameController gameController;
+    private InputManager inputManager;
+    private SceneManager sceneManager;
+    public void main(String[] args) {
+
+    }
     
 }
